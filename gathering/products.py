87c1d220c25a33b642818dd13146c6ba84badc91
jgroeschen <josephgroeschen@gmail.com>
2023-02-09 11:28:57 -0500

    Added id-based product search

--- gathering/products.py
+++ gathering/products.py
@@ -33,6 +33,49 @@ def get_products(token: str,
         'filter.limit': limit,
     }
     response = requests.get('https://api.kroger.com/v1/products',
+                            params=params, headers=headers)
+    if response.ok:
+        product_data = response.json()
+    else:
+        product_data = []
+    return product_data
+
+
+def get_products_by_id(token: str,
+                 locID: str = '01400513',
+                 productID: str = '',
+                 ) -> dict:
+    '''Return a dictionary of products from a search.
+
+    Retrieves data for a/some product id(s) and returns the JSON-formatted 
+    result as a dictionary.
+
+    Args:
+        token: A string from get_token().
+        locID: A string that can be user-provided or from get_locations().
+        productID: A string that can be user-provided or from upcs.txt. May be
+          one product id or multiple, comma-separated.
+
+
+    Returns:
+        A dictionary containing JSON data of product information from search
+        results.
+    '''
+
+    headers = {
+        'Accept': 'application/json\\',
+        'Authorization': 'Bearer ' + token,
+    }
+
+    params = {
+    "filter.locationId": locID,
+    "filter.productId": productID,
+    "filter.fulfillment": "ais",
+    }
+
+    
+
+    response = requests.get("https://api.kroger.com/v1/products",
                         params=params, headers=headers)
     if response.ok:
         product_data = response.json()
